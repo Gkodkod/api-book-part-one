b51de401bb55677419384464bbfc9a596ec73846
Gal Levinshtein <10193612+Gkodkod@users.noreply.github.com>
2026-02-02 12:51:40 -0500

    Containerize chapter 6 and update configuration

--- chapter5/complete/database.py
+++ chapter5/complete/database.py
@@ -3,10 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fantasy_data.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./fantasy_data.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/fantasy_data"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
